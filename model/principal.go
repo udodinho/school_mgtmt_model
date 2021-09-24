ae63f2947143902dd119882119edc72722277d01
udodinho <udodinho200@gmail.com>
2021-09-24 16:24:26 +0100

    update

--- model/principal.go
+++ model/principal.go
@@ -1,5 +1,7 @@
 package model
 
+import "fmt"
+
 type Person struct {
 	FirstName string
 	LastName string
@@ -26,4 +28,11 @@ var applicantBio = applicants.ApplicantsBio
 
 func (p Principal) Admin() string {
 	returnString := ""
+	for _, val := range applicantBio {
+		if val.Age >= 18  {
+			//fmt.Printf("Congratulations %v you have gain a provisional admission\n", val)
+			returnString = fmt.Sprintf("Congratulations %v you have gain a provisional admission\n", val)
+		}
+	}
+	return returnString
 }

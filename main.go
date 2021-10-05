8e637f81dd31c8dba0a55b43fb21c17f0714cacd
udodinho <udodinho200@gmail.com>
2021-10-05 10:43:17 +0100

    created main file

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "school_mgmt/model/principal"
+import (
+	"school_mgmt/model/applicant"
+	"school_mgmt/model/principal"
+	"school_mgmt/model/student"
+	"school_mgmt/model/teacher"
+)
 
 var headAdmin  =  principal.Principal {
 	Details: principal.Person{
@@ -11,3 +16,14 @@ var headAdmin  =  principal.Principal {
 	},
 	Rank: "Vice Chancellor",
 }
+
+var seniorLecturer = teacher.Teachers{
+	FirstName: "Alex",
+	LastName:  "Okocha",
+	Age:       40,
+	Identity:  "Lecturer",
+}
+
+var StudentProfile = student.StudentProfile
+
+var applicantBio = applicant.ApplicantsBio

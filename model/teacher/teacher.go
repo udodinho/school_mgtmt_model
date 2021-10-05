653fbf6b237860d12eb177e0d906958f69e5a080
udodinho <udodinho200@gmail.com>
2021-10-05 10:15:08 +0100

    teacher model for grading

--- model/teacher/teacher.go
+++ model/teacher/teacher.go
@@ -1,6 +1,9 @@
 package teacher
 
-import "school_mgmt/model/student"
+import (
+	"fmt"
+	"school_mgmt/model/student"
+)
 
 type Teachers struct {
 	FirstName string
@@ -15,4 +18,21 @@ var seniorLecturer = Teachers{
 	LastName:  "Okocha",
 	Age:       40,
 	Identity:  "Lecturer",
+}
+
+
+func (t Teachers) Grading() string{
+	for _, val := range student.StudentProfile {
+		if val.Score >= 70 {
+			fmt.Printf("Congrats %v you got an A in your examination\n", val)
+		} else if val.Score >= 50 && val.Score <= 69 {
+			fmt.Printf("Congrats %v you got a B in your examination\n", val)
+		} else if val.Score >= 40 && val.Score <= 49 {
+			fmt.Printf("Congrats %v you got a C in your examination\n", val)
+		} else {
+			fmt.Printf("%v You got an F in your examination\n", val)
+		}
+	}
+
+	return ""
 }
